39d8faaee4d2322c24fa56040c81e15b3d63c97b
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-19 15:55:28 +0100

    add more function to the jquery to prevent default

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
-      $(".person1").append("blah blah");
-      $(".person2").append("blah blah");
-      $(".animal").append("blah blah");
-      $(".exclamation").append("blah blah");
-      $(".verb").append("blah blah");
-      $(".noun").append("blah blah");
+      const person1 = $("#person1").val();
+      const person2 = $("#person2").val();
+      const animal= $("#animal").val();
+      const exclamation = $("#exclamation").val();
+      const verb = $("#verb").val();
+      const noun = $("#noun").val();
+  
+      $(".person1").append(person1);
+      $(".person2").append(person2);
+      $(".animal").append(animal);
+      $(".exclamation").append(exclamation);
+      $(".verb").append(verb);
+      $(".noun").append(noun);
   
       $("#story").show();
-
+  
       event.preventDefault();
     });
   });
